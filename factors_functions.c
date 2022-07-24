500ec2a89e2aa4524d977ef5037bf9b609752b79
NatnaelFY <natfiky2020@gmail.com>
2022-07-25 01:57:05 +0300

    RSA challenge

---
+++ factors_functions.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * trial_division - Trial dividing
+ * Return: 1 smallest divisor if found, if n is prime
+ * @n: - integer numbers
+ */
+
+
+
+int trial_division(long int n)
+{
+	long int f;
+
+
+	if (n % 2 == 0)
+	{
+		printf("%lu = %lu * %i\n", n, n / 2, 2);
+		return (0);
+	}
+	f = 3;
+	while (f * f <= n)
+	{
+		if (n % f == 0)
+		{
+			printf("%lu = %lu * %lu\n", n, n / f, f);
+			return (0);
+		}
+		else
+		{
+			f += 2;
+		}
+	}
+	printf("%lu = %lu * %i\n", n, n, 1);
+	return (0);
+}
